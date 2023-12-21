c8d43151f22c2068e7c5ec31b8e4aa26bdab7bfb
Sara <sala0002@stud.kea.dk>
2023-12-21 17:53:49 +0100

    2

---
+++ script.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  const slides = document.querySelectorAll(".slides img");
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+}
