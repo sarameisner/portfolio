6ba527ed0ed510499108083518a4361ce0dbf09f
Sara <sala0002@stud.kea.dk>
2024-01-02 20:19:52 +0100

    8

--- menu.js
+++ menu.js
@@ -1,5 +1,7 @@
 function toggleMenu() {
+  // Finder id'et menuContainer
   var x = document.getElementById("menuContainer");
+  // Her tjekker vi om menuen allerede er synlig, hvis den er, skjuler denne kode menuen. Hvis den ikke er synlig, viser koden menuen.
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
