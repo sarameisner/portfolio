a2422eb3b3841cdb73d6b89470ec023264cb8a10
Sara <sala0002@stud.kea.dk>
2024-01-04 19:00:09 +0100

    11

--- popup.js
+++ popup.js
@@ -1,23 +1,23 @@
-// Get the modal
+// henter id'et
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
+// henter knappen der åbner pop up vinduet
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
+// henter "<span>" som lukker pop up vinduet
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+// gør at når der klikkes så åbner pop up vinduet
 btn.onclick = function () {
   modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
+// når der klikkes på <span> (x) så lukker vinduet
 span.onclick = function () {
   modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
+// når der bliver klikket uden fra pop up vinduet lukkes det
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
