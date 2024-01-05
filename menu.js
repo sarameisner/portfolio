4a0a502e2c8a464477f90bdd680ac8713bc74169
Sara <sala0002@stud.kea.dk>
2024-01-05 12:18:41 +0100

    14

--- menu.js
+++ menu.js
@@ -1,6 +1,8 @@
+// gør så man kan klikke på den der hedder menu
+document.querySelector("#menu").addEventListener("click", toggleMenu);
 function toggleMenu() {
-  // Finder id'et menuContainer
-  var x = document.getElementById("menuContainer");
+  // Finder id'et menudrop
+  let x = document.getElementById("menudrop");
   // Her tjekker vi om menuen allerede er synlig, hvis den er, skjuler denne kode menuen. Hvis den ikke er synlig, viser koden menuen.
   if (x.style.display === "block") {
     x.style.display = "none";
