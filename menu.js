56e6c200eb4a1e4aa5d579f7829b570b3306378b
Sara <sala0002@stud.kea.dk>
2024-01-09 22:16:50 +0100

    sidste rettelser

--- menu.js
+++ menu.js
@@ -1,9 +1,9 @@
 // gør så man kan klikke på den der hedder menu
 document.querySelector("#menu").addEventListener("click", toggleMenu);
 function toggleMenu() {
-  // Finder id'et menudrop
+  // finder id'et menudrop
   let x = document.getElementById("menudrop");
-  // Her tjekker vi om menuen allerede er synlig, hvis den er, skjuler denne kode menuen. Hvis den ikke er synlig, viser koden menuen.
+  // her tjekker den om menuen allerede er synlig, hvis den er, skjuler denne kode menuen. hvis den ikke er synlig, vises menuen
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
